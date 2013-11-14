90d07c251294dff5075c53aac9cebb82a225bd19
Mark Feedly <markfeedly@gmail.com>
2013-11-14 12:54:42 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VirtualMuseum::Application.config.secret_key_base = '7dbfeb4ae12bf929a816067ec9d64032ff511d452838a76c728cafc01be8ed5ccb84f11e5e38dcf10a59c06f2eb20167a64d9178fbe80a54e11cf2ecf203b0a6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VirtualMuseum::Application.config.session_store :cookie_store, key: '_virtual-museum_session'
