9ce3e7d76303d7495a6b19d01049f3a73709cb4b
Mark Feedly <markfeedly@gmail.com>
2013-11-23 23:50:54 +0000

    edit pages, still to improve tests - done in advance of tests

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,5 +7,18 @@ class PagesController < ApplicationController
   end
   def show
     current_slug = request.path_info.sub(/.*\//,'')
-    @page = Page.find_by slug: current_slug  end
+    @page = Page.find_by slug: current_slug
+    @edit_url = '/pages/'+current_slug+'/edit'
+  end
+  def edit
+    current_slug = $1 if request.path_info =~ /.*\/(.*)\/.*/
+    @page = Page.find_by slug: current_slug
+    @update_url= '/pages/'+current_slug
+  end
+  def update
+    current_slug = request.path_info.sub(/.*\//,'')
+    page = Page.find_by slug: current_slug
+    page.update(title: params[:title], content: params[:content])
+    redirect_to '/pages/'+current_slug, status: 301
+  end
 end
