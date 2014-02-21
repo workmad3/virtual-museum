bda994eb19cece5aa7752ae4aa45ef72a30b704b
Mark Feedly <markfeedly@gmail.com>
2014-02-21 19:11:31 +0000

    got page state spec working

--- spec/models/page_state_spec.rb
+++ spec/models/page_state_spec.rb
@@ -5,20 +5,30 @@ RSpec.configure do |config|
 end
 
 describe 'Previous Page' do
-    before(:each) do
-      user
-      page
-      @previous_page = FactoryGirl.create(:page_state, page: page, user: user)
-    end
 
-  it { @previous_page.should validate_presence_of(:page_id) }
-  it { @previous_page.should belong_to(:page) }
+  def user() @user end
+  def page() @page end
+  def page_state() @page_state end
 
-  it { @previous_page.should validate_presence_of(:user_id) }
-  it { @previous_page.should belong_to(:user) }
+  before(:each) do
+    @user = FactoryGirl.create(:user)
+    @page = FactoryGirl.create(:page)
+    @page_state = FactoryGirl.create(:page_state, title: page.original_title, content: 'the content',
+                                     user: user, page: page)
+  end
 
-  it 'should do something' do
+  it { page_state.should validate_presence_of(:page_id) }
+  it { page_state.should belong_to(:page) }
 
-    true.should == false
+  it { page_state.should validate_presence_of(:user_id) }
+  it { page_state.should belong_to(:user) }
+
+  it "should return the correct title" do
+    page_state.title.should == page.original_title
+  end
+
+
+  it "should return the correct content" do
+    page_state.content.should == 'the content'
   end
 end
