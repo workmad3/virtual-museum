7861ccf7b639670b5093e62ac2e2fe6438b297b7
Mark Feedly <markfeedly@gmail.com>
2014-02-28 23:07:13 +0000

    getting near end of LinkInterpreter dev and tests

--- lib/parsing/link_interpreter.rb
+++ lib/parsing/link_interpreter.rb
@@ -5,7 +5,8 @@ class LinkInterpreter
 
   def initialize text
     @text = text.strip
-    @first, @rest = text.split(/ /, 2)
+    @first, @rest = @text.split(/ /, 2)
+    @rest.strip! if @rest
   end
 
   def url?
@@ -22,12 +23,13 @@ class LinkInterpreter
   def valid_host?
     !(@uri.host.nil? || @uri.host.include?(".."))
   end
+
   def url_suffix?
     url? && ((@first =~ /\/\/[^\/]+\/[^\/]+/) != nil)
   end
 
   def image_url?
-    url_suffix? && ((@first =~ /\.png/) || (@first =~ /\.jpg/))
+    url_suffix? && ((@first =~ /\.png$/) || (@first =~ /\.jpg$/))
   end
 
   def domain
@@ -51,16 +53,48 @@ class LinkInterpreter
     is_domain? 'vimeo.com'
   end
 
-  def process_bracket_contents
+  def process_page_title
+    pg = Page.find_by_title @text
+    if pg != [] #TODO make find_by_title mroe friendly if page not found
+      "<a href='/pages/#{pg.slug}' data-page>#{@text}</a>"
+    else
+      "<a href='/pages/new?page_title=#{@text}' data-new-page>#{@text}</a>"
+    end
   end
 
-  def process_page_title  page_title
-    pg = Page.find_by_title page_title
-    if pg
-      "<a href='/pages/#{pg.slug}' style='color: #0000FF'>#{page_title}</a>"
-    else
-      "<a href='/pages/#{pg.slug}/new' style='color: #0000FF'>#{page_title}</a>"
-    end
+  def process_url_without_text
+    "<a href='#{@text}' external-link>#{@text}</a>"
+  end
+
+  def process_url_with_text
+    "<a href='#{@first}' external-link>#{@rest}</a>"
+  end
+
+#TODO untested
+  def process_url
+    @rest == '' ? process_url_without_text : process_url_with_text
+  end
+
+  def process_image_url_without_width
+    "<div><img href='#{@text}'></div>"
+  end
+
+  def process_image_url_with_width
+    "<div><img href='#{@first}' style='width: #{@rest}px;'></div>"
+  end
+
+#TODO untested
+  def process_image_url
+    @rest == '' ? process_image_url_without_width : process_image_url_with_width
+  end
+
+#TODO untested
+  def process_bracket_contents
+    return process_page_title if !url?
+    return process_image_url if image_url?
+    return 'youtube' if is_youtube_url?
+    return 'vimeo' if is_vimeo_url?
+    return process_image_url if image_url?
   end
 
 
