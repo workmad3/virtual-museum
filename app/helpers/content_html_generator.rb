ae09f8f9419639c5514ced79fa5931fcca4d9171
Mark Feedly <markfeedly@gmail.com>
2014-03-02 20:46:09 +0000

    minor refactor app/helpers/content_generator.rb to simplify code there

--- app/helpers/content_html_generator.rb
+++ app/helpers/content_html_generator.rb
@@ -2,18 +2,9 @@
 require 'parsing/content_parser'
 require 'parsing/content_transformer'
 
-
 class ContentHtmlGenerator
-  def self.generate  pg
-    parsed = ContentParser.new.parse(pg.content)
-    pp '----------------------------------------------------'
-    pp parsed
-    pp '----------------------------------------------------'
-    output_array = ContentTransformer.new.apply(parsed)
-    output_string = ''
-    output_array.each { |str| output_string = output_string + str }
-    pp output_string
-    pp '----------------------------------------------------'
-    output_string
+  def self.generate(page)
+    parsed = ContentParser.new.parse(page.content)
+    ContentTransformer.new.apply(parsed).join(' ')
   end
 end
