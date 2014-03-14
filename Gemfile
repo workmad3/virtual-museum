b2cce886327c63d4a709cf5de12c3a3ea08dbfe5
Mark Van <markfeedly@gmail.com>
2014-03-14 23:41:42 +0000

    decent exposreu used for page controllers and views

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'rolify'
 gem 'simple_form'
 gem 'friendly_id', '~> 5.0.0' # Note: You MUST use 5.0.0 or greater for Rails 4.0+
 gem 'parslet'
+gem 'decent_exposure'
 
 group :development, :test do
   gem 'better_errors'
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,31 +1,27 @@
 class PagesController < ApplicationController
+  expose(:current_slug) { request.path_info.gsub(/.*pages\//,'').gsub(/\/.*/,'') }
+  expose(:page)         { Page.find_by :slug => current_slug }
+  expose(:edit_url)     { '/pages/'+current_slug+'/edit' }
+  expose(:update_url)   { '/pages/'+current_slug }
+
+
   def new
-    @new_title=params[:title]
   end
 
   def create
-
     p = Page.create(original_title: params[:title])
     PageState.create(page_id: p.id,
                              user_id: current_user.id, title: params[:title], content: params[:content])
-    redirect_to '/pages/' + p.slug, status: 301
+    redirect_to '/pages/'+p.slug, status: 301
   end
 
   def show
-    current_slug = request.path_info.sub(/.*\//, '')
-    @page = Page.find_by slug: current_slug
-    @edit_url = '/pages/'+current_slug+'/edit'
   end
 
   def edit
-    current_slug = $1 if request.path_info =~ /.*\/(.*)\/.*/
-    @page = Page.find_by slug: current_slug
-    @update_url= '/pages/'+current_slug
   end
 
   def update
-    current_slug = request.path_info.sub(/.*\//, '')
-    page = Page.find_by slug: current_slug
     page.change(current_user, params)   # TODO change to Page#change to turn on page history
     redirect_to '/pages/'+current_slug, status: 301
   end
