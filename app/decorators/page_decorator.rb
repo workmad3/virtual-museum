96605973322d14bdb13352edc5f2530b9776fdff
Mark Van <markfeedly@gmail.com>
2014-04-05 18:41:04 +0100

    continuing the change to haml, still have to deal w floated edit button

--- app/decorators/page_decorator.rb
+++ app/decorators/page_decorator.rb
@@ -1,58 +1,25 @@
 class PageDecorator < Draper::Decorator
   delegate_all
 
-  def tab_nav
-    h.content_tag :ul, id: 'tabs', class: 'nav nav-tabs', 'data-tabs'.to_sym => '"tabs" 'do
-      ( h.content_tag :li, class: 'active' do
-          h.content_tag :a, href: '#content_tab', 'data-toggle'.to_sym => 'tab' do 'Page' end
-      end ) +
-      ( h.content_tag :li do
-          h.content_tag :a, href: '#history_tab', 'data-toggle'.to_sym => 'tab' do 'History' end
-      end ) +
-      ( h.content_tag :li do
-          h.content_tag :a, href: '#last_change_tab', 'data-toggle'.to_sym => 'tab' do 'Last Change' end
-      end )
+  # contents tab
+
+  def content_as_html(thing)
+    ContentHtmlGenerator.generate(thing).html_safe
+  end
+
+  def tags_as_html
+    if model.tags.empty?
+      "No tags"
+    else
+      model.tags.map{|t| h.link_to(t, "/tags/#{t}")}
     end
   end
 
-  def content_tab(signed_in, e_url, vv)
-    h.content_tag :div, id: 'content_tab', class: 'tab-pane active' do
-      ( h.content_tag :div, class: 'page-width' do
-        ( h.content_tag :div, class: 'content_block' do
-
-          ( h.content_tag :h1, id: "top" do
-              page.title
-            end ) +
-          ( h.content_tag :div, class: 'content_content' do
-              content_as_html(model)
-          end ) +
-          ( h.content_tag :div do
-            (  h.content_tag :div, class:'content_tags' do
-                h.render 'tags/tags', tags: model.tags
-            end ) +
-            (  h.content_tag :div, class:'content-edit' do
-              edit_button_as_html(signed_in, e_url)
-            end )
-          end)
-        end)
-      end)  +
-      #edit_button_if(signed_in, e_url) +
-      comment_heading +
-      ( h.content_tag :div, class: 'convenient-box' do
-        ( h.content_tag :div, class: 'comment-block' do
-            new_comment_if(signed_in)
-        end ) +
-        comments_as_html
-      end )
-    end
-
+  def edit_button_as_html(signed_in, edit_url)
+    signed_in ? h.link_to("Edit", edit_url, class: "btn btn-primary") : ''
   end
 
-  def comment_heading
-    h.content_tag :h2 do 'Comments' end
-  end
-
-  def new_comment_if(signed_in)
+  def new_comment_form_as_html(signed_in)
     if signed_in
       h.content_tag :div, class: 'comment-form' do
         h.render :partial => 'layouts/comment_form'
@@ -70,53 +37,13 @@ class PageDecorator < Draper::Decorator
     end
   end
 
-
-  def history_tab
-    h.content_tag :div, id: 'history_tab', class: 'tab-pane' do
-      h.content_tag :ul, 'no-bullets' => true do
-        h.render(partial: 'pages/page_state', collection: model.history.reverse)
-      end
-    end
-  end
-
-  def last_change_tab
-    h.content_tag :div, id: 'last_change_tab', class: 'tab-pane' do
-      h.content_tag :ul, 'no-bullets' => true do
-        last_change_as_html
-      end
-    end
-  end
+# history tab
 
   def history_as_html
     h.render(partial: 'pages/page_state', collection: model.history.reverse)
   end
 
-  def tags_as_html
-    if model.tags.empty?
-                "No tags"
-              else
-                model.tags.map{|t| h.link_to(t, "/tags/#{t}")}
-              end
-
-=begin
-    #tag_links =
-
-    if model.tags
-      tags = model.tags
-      tarr = tags.split(',').collect{|t| t.strip!; " <a href='/tags/#{t}'>#{t}</a>"}
-      out = 'Tags: '+tarr.join(', ')+'<br/><br/>'
-      out.html_safe
-    else
-      'No tags<br/><br/>'.html_safe
-    end
-=end
-  end
-
-
-  def edit_button_as_html(signed_in, edit_url)
-    signed_in ? h.link_to("Edit", edit_url, class: "btn btn-primary") : ''
-  end
-
+# last change tab
 
   def last_change_as_html
     previous_content = page.previous_content
@@ -127,11 +54,5 @@ class PageDecorator < Draper::Decorator
       'No previous edit'
     end
   end
-
-  def content_as_html(thing)
-    ContentHtmlGenerator.generate(thing).html_safe
-  end
-
-
 
 end
