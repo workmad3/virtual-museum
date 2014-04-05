16662d54aa0f83178ee48dc85dfadb1648958640
Mark Van <markfeedly@gmail.com>
2014-04-05 22:52:06 +0100

    for now done formatting and css refactoring and move to more haml

--- app/assets/javascripts/page.js
+++ app/assets/javascripts/page.js
@@ -1,3 +1,9 @@
 jQuery(document).ready(function ($) {
     $('#tabs').tab();
     });
+
+function hide_and_show() {
+    event.preventDefault();
+    document.getElementById('new-comment-form-to-show').className = '';
+    document.getElementById('button-to-hide').className = 'hiddenx';
+}
