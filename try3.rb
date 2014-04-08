9c2c6c2cceea884c1b568dc24829d1524a7b982a
Mark Van <markfeedly@gmail.com>
2014-04-08 16:31:07 +0100

    potential metaprogramming solution

--- try3.rb
+++ try3.rb
@@ -1,25 +1,45 @@
 class X
   #attr_accessor :x, :y
 
-  [
-    ['x', Proc.new{|val| val+100;}],
-    ['y', Proc.new{|val| val.downcase;}]
-  ].each do |name, proc|
-    define_method("#{name}="){|v| instance_variable_set("@#{name}", proc.call(v))}
+  def attr_my_accessor(args)
+    args.each do |name, proc|
+      self.class.send :define_method, "#{name}", Proc.new{instance_variable_get("@#{name}") }
+      self.class.send :define_method, "#{name}=", Proc.new { |rhs_of_equals|
+        instance_variable_set("@#{name}", proc.call(rhs_of_equals))
+      }
+    end
   end
 
   def initialize
+    attr_my_accessor(x: lambda {|val| val+100;}, y: lambda {|val| val.downcase;})
+
     @x='hi'
     @y='Si'
+  end
+  def setit
+    @y = 'Mark'
   end
   def print
     puts "#{@x} #{@y}"
   end
 end
 
-x=X.new
-x.x = 3
-x.y = '-----'
-x.print
+o=X.new
+puts 'after initialisation'
+o.print
+puts 'set o.x to 3'
+o.x = 30000
+
+o.print
 
 
+
+=begin
+  [
+    ['x', lambda {|val| val+100;}],
+    ['y', lambda {|val| val.downcase;}]
+  ].each do |name, proc|
+    define_method("#{name}="){|v| instance_variable_set("@#{name}", proc.call(v)) }
+    define_method("#{name}") {    instance_variable_get("@#{name}") }
+  end
+=end
