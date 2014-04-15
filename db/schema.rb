4d238363462d54b52c33ec05e254a7ff8f108a4f
Mark Van <markfeedly@gmail.com>
2014-04-15 08:20:19 +0100

    removed timestamp from join table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140414155657) do
+ActiveRecord::Schema.define(version: 20140415070357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,10 +42,8 @@ ActiveRecord::Schema.define(version: 20140414155657) do
   add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
 
   create_table "page_resource_uses", force: true do |t|
-    t.integer  "page_id"
-    t.integer  "resource_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer "page_id"
+    t.integer "resource_id"
   end
 
   create_table "page_states", force: true do |t|
