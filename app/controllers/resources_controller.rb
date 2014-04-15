c2ba73e773a0855ae7fdbc6cbc0b406a70ecc5f0
Mark Van <markfeedly@gmail.com>
2014-04-15 20:53:05 +0100

    got multiple selections working on resource editing page

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -25,11 +25,16 @@ class ResourcesController < ApplicationController
   end
 
   def update
+    selected_pages = params['resource']['resource_usages']
+    selected_pages = [] if selected_pages == nil
+    selected_pages = [selected_pages] if selected_pages.class != Array
+    selected_pages = selected_pages.delete_if{|s| s == ''}
+    selected_pages = selected_pages.collect{|s| Page.find_by_id(Integer(s)) }
     # this assigns attributes using setters that are either provided by active record or explicitly
     if resource.update_attributes(file: params['resource']['file'],
                                   description: params['resource']['description'],
                                   title: params['resource']['title'],
-                                  page_id: params['resource']['page_id']
+                                  pages: selected_pages
                                   )
       redirect_to resource_url(resource), status: 301
     else
