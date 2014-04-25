eb5f994842208ab9ee8e3f53dab0b89483547984
Mark Van <markfeedly@gmail.com>
2014-04-25 23:03:35 +0100

    twitter omniauth done plus some signup form etc ui layout

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,10 +1,16 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def twitter
-    raise request.env["omniauth.auth"].to_yaml
+    user = User.process_omniauth(request.env["omniauth.auth"])
+    if user.persisted?
+      sign_in user
+      flash[:notice] = 'Signed in successfully with Twitter'
+      redirect_to root_path, status: 301
+    else
+      session["devise.user_attributes"] =
+          user.attributes
+      redirect_to new_user_registration_url
+    end
   end
 
-  def failure
-    xxxxxxx
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ActiveRecord::Base
-  #rolify
 
   include Authority::UserAbilities
 
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:twitter]
@@ -13,13 +12,16 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :resources
 
-  def self.find_or_create_for_twitter_oauth(auth)
-    where(auth.slice(:provider, :uid)).find_or_create do |user|
+  # -----------------------------------------------------------
+
+  def self.process_omniauth(auth)
+    User.where(auth.slice(:provider, :uid)).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
-      user.email = auth.info.email
+      user.email = 'a@b.c'# auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
+      user.password_confirmation = user.password
+      user.name = auth.info.nickname
       # user.image = auth.info.image # assuming the user model has an image
     end
   end
